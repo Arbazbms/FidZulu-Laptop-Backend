5f71509a12293de2e86d7853418ecbafc15029c7
Saif Basheer Ahamed <saif191020@gmail.com>
2022-11-07 10:46:19 +0530

    added Location based Pricing

--- routes/laptops.js
+++ routes/laptops.js
@@ -6,25 +6,48 @@ var router = express.Router();
 
 
 let read_json_file = () => {
-  let file = './data/Laptopsjson.json';
-  return fs.readFileSync(file);
+    let file = './data/Laptopsjson.json';
+    return fs.readFileSync(file);
 }
 
 exports.list = () => {
-  return JSON.parse(read_json_file());
+    return JSON.parse(read_json_file());
 };
 
+function computePrice(json, percent) {
+    percent = percent / 100;
+    for (let laptop of json) {
+        console.log(laptop.price);
+        laptop.price = laptop.price * (1 + percent)
+        console.log(laptop.price);
+    }
+    console.log(json);
+    return json
+}
 
 
 /* GET users listing. */
-router.get('/all', function(req, res, next) {
-  
-let json_result = JSON.parse(read_json_file());
-  res.json(json_result);
+router.get('/all/:location?', function(req, res, next) {
+    let json_result = JSON.parse(read_json_file());
+
+    let location = req.params.location;
+    if (location) {
+        location = location.toLocaleLowerCase();
+        console.log(`recieved location: ${location}`);
+        if (location === 'raleigh') {
+            json_result = computePrice(json_result, 7.5);
+        } else if (location === 'durham') {
+            console.log("TEST");
+            json_result = computePrice(json_result, 8);
+        } else {
+            console.log('not a valid locatioon');
+        }
+    }
+    res.json(json_result);
 });
 
-router.get('/team', function(req, res, next){
-  res.json(team)
+router.get('/team', function(req, res, next) {
+    res.json(team)
 })
 
 module.exports = router;

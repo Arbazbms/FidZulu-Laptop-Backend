65a7618e619ee17ec330d90f26d2787835cf3e22
Tanmay Rode <v.tanmay13@gmail.com>
2022-11-07 11:26:43 +0530

    Added positive tests for all end points

--- spec/laptopTest_spec.js
+++ spec/laptopTest_spec.js
@@ -8,9 +8,42 @@ describe("Test server for Laptops", () => {
     describe("GET /team", () => {
         it("returns Team Name & Members", (done) => {
             request.get(baseURL+"team", (err, resp, body) => {
+                teamData = JSON.parse(body);
+                expect(teamData.team).toBe("team09-laptops");
+                expect(teamData.membersNames[1]).toBe("Tanmay");
+                done();
+            });
+        });
+    });
+
+    describe("GET /all/**", () => {
+        it("returns regular prices of the laptops", (done) => {
+            request.get(baseURL+"all", (err, resp, body) => {
                 laptopData = JSON.parse(body);
-                console.log(laptopData);
-                expect(laptopData.team).toBe("team09-laptops");
+                expect(laptopData[0].product).toBe("ThinkPad T430s");
+                expect(laptopData[0].price).toBe(325.09);
+                done();
+            });
+        });
+    });
+
+    describe("GET /all/raleigh", () => {
+        it("returns laptop prices after a 7.5% sales-tax addition", (done) => {
+            request.get(baseURL+"all/raleigh", (err, resp, body) => {
+                laptopData = JSON.parse(body);
+                expect(laptopData[0].product).toBe("ThinkPad T430s");
+                expect(laptopData[0].price).toBe(349.47175);
+                done();
+            });
+        });
+    });
+
+    describe("GET /all/durham", () => {
+        it("returns laptop prices after an 8% sales-tax addition", (done) => {
+            request.get(baseURL+"all/durham", (err, resp, body) => {
+                laptopData = JSON.parse(body);
+                expect(laptopData[0].product).toBe("ThinkPad T430s");
+                expect(laptopData[0].price).toBe(351.0972);
                 done();
             });
         });
